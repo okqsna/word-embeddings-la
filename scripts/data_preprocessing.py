91135d9711c7244211b114654cbaf089735cb9cc
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2026-04-04 15:47:20 +0300

    transliteracy to be fixed

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,5 +1,33 @@
+"""
+Preprocessing billingual dictionary 
+for exploring word embeddings in context
+of Ukrainian-English translation
+"""
 import csv
 import re
+
+from translitua import (
+    translit,
+    UkrainianKMU,
+    UkrainianSimple,
+    UkrainianWWS,
+    UkrainianBritish,
+    UkrainianBGN,
+    UkrainianISO9,
+    UkrainianPassport2007,
+    UkrainianNational1996,
+)
+
+TRANSLIT_SYSTEMS = [
+    UkrainianKMU,
+    UkrainianSimple,
+    UkrainianWWS,
+    UkrainianBritish,
+    UkrainianBGN,
+    UkrainianISO9,
+    UkrainianPassport2007,
+    UkrainianNational1996,
+]
 
 def clean_word(word: str)->str:
     """
@@ -27,6 +55,16 @@ def check_word_language(word: str, language: str)->bool:
             return bool(re.fullmatch(r"[a-z]+(?:[-'][a-z]+)*", word))
     return False
 
+def convert_transliteracy(word: str) -> list:
+    """
+    Function finds all transliteracy variant of the Ukrainian word.
+
+    :param word: str, word to transliterate
+    :return: list, transliteration results
+    """
+    return [translit(word, system) for system in TRANSLIT_SYSTEMS]
+
+
 def write_res_csv(data_dictionary: dict, path: str)-> None:
     """
     Function writes the cleaned words into
@@ -42,9 +80,9 @@ def write_res_csv(data_dictionary: dict, path: str)-> None:
         for ua, eng_collection in data_dictionary.items():
             for eng in eng_collection:
                 writer.writerow([ua, eng])
-    
-def data_preprocessing(original_dictionary: str="../data/original/uk-en-full.txt",
-                       cleaned_dictionary: str="../data/usage/uk-en-full.csv") -> dict:
+
+def data_preprocessing(original_dictionary: str="../data/original/uk-en-train.txt",
+                       cleaned_dictionary: str="../data/usage/uk-en-train.csv") -> dict:
     """
     Function cleans the original English-Ukrainian dictionary
     and forms new dictionary with word pairs only.
@@ -56,7 +94,7 @@ def data_preprocessing(original_dictionary: str="../data/original/uk-en-full.txt
     result_dictionary = {}
     with open(original_dictionary, "r", encoding="utf-8") as f:
         data = f.readlines()
-    
+
     for line in data:
         line = line.strip("\n")
         if not line:
@@ -64,24 +102,18 @@ def data_preprocessing(original_dictionary: str="../data/original/uk-en-full.txt
 
         ukrainian_word, english_word = line.split()
 
-        ukrainian_word.lower()
-        english_word.lower()
+        ukrainian_word = ukrainian_word.lower()
+        english_word = english_word.lower()
 
         ukrainian_word = clean_word(ukrainian_word)
         english_word = clean_word(english_word)
 
-
-        if check_word_language(ukrainian_word, "ua") == False:
-            print(ukrainian_word)
-
-        
-        if check_word_language(english_word, "en") == False:
-            print(english_word)
-
         if check_word_language(ukrainian_word, "ua") and check_word_language(english_word, "en"):
             result_dictionary.setdefault(ukrainian_word, [])
-            result_dictionary[ukrainian_word].append(english_word)  
-    
+            result_dictionary[ukrainian_word].append(english_word)
+
+        transliterated_ukrainian_word = convert_transliteracy(ukrainian_word)
+
     write_res_csv(result_dictionary, cleaned_dictionary)
     return result_dictionary
 
